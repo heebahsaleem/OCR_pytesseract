12c0396ff88658cdc1996909544f0387eb01dd52
Heebah Saleem <saleem.heebah@yahoo.com>
2024-01-14 21:59:11 +0900

    Add files via upload

---
+++ OCR.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import pytesseract
+import argparse
+import cv2
+import os
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import nltk
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
